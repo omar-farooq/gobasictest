d6aba40759e6ceee98068752f468b31776fd68cd
Omar Farooq <wavygravy@outlook.com>
2024-11-09 23:10:40 +0000

    add kustomization

--- gohelloworld/hello.go
+++ gohelloworld/hello.go
@@ -11,9 +11,13 @@ func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Welcome to omar's site")
 }
 
+func logHandler(w http.ResponseWriter, r *http.Request) {
+    log.Println("this is a log to view")
+}
 
 func main() {
     http.HandleFunc("/", handler)
+    http.HandleFunc("/log", logHandler)
     http.Handle("/metrics", promhttp.Handler())
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
